06dd6f2c3c0b767b9f543365d061fc73d713c8ad
labhvam5 <labhvamkrsharma@gmail.com>
2024-05-19 23:13:11 +0530

    adding request_models and POST request

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from starlette import status
 from models import Todos
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from pydantic import BaseModel
+from request_models import TodosRequest
 
 app = FastAPI()
 
@@ -21,17 +21,26 @@ def get_db():
 
 db_dependency: Session = Depends(get_db)
 
+
 @app.get('/todos', status_code=status.HTTP_200_OK)
 async def get_all(db = db_dependency):
     todos = db.query(Todos).all()
     return todos
 
+
 @app.get('/todos/{todo_id}', status_code=status.HTTP_200_OK)
-async def get_todo_by_id(todo_id: int= Path(gt=0), db: Session = db_dependency):
+async def get_todo_by_id(todo_id: int= Path(gt=0), db = db_dependency):
     todo = db.query(Todos).filter(Todos.id == todo_id).first()
     if not todo:
         raise HTTPException(status_code=404, detail="Todo not found")
     return todo
 
-# @app.post('/todos')
-# async def create_todo(todo: Todos, db: Session = db_dependency):
+
+@app.post('/todos', status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_request: TodosRequest, db = db_dependency):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
+    db.refresh(todo_model)
+    # Return the newly created todo
+    return todo_model
---
+++ request_models.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+
+class TodosRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    completed: bool
